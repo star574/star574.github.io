887b5ebe244ae2b2af403412e20901f6d7352214
star574 <shihengluo574@gmail.com>
2022-10-03 01:47:38 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+import { defaultTheme, defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: "star574's blog",
+    description: "star574's blog",
+    base: "/",
+    head: [
+        [
+            'link', { rel: 'icon', href: '/images/logo.png' }
+        ]
+    ],
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: '首页',
+                link: '/',
+            },
+            {
+                text: '首页',
+                link: '/',
+            },
+            {
+                text: '首页',
+                link: '/',
+            },
+            {
+                text: '首页',
+                link: '/',
+            },
+        ]
+    })
+})
---
+++ docs/index.md
@@ -0,0 +1,7 @@
+---
+lang: zh-CN
+title: 第一篇blog
+description: 第一篇blog
+---
+
+# hello wrold
